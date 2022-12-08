189b43db8416f2e182c626b35a11746e53465366
Yassine Ait Ouaddi <98220932+aitouadd@users.noreply.github.com>
2022-12-08 18:42:55 +0100

    Add files via upload

---
+++ Project #21 - Simple Drag & Drop/script.js
@@ -0,0 +1,44 @@
+const lists = document.querySelectorAll(".list");
+const listItems = document.querySelectorAll(".list-item");
+
+  let draggedItem = null;
+
+for(let a = 0 ; a < listItems.length ; a++ ){
+     const item = listItems[a];
+
+     item.addEventListener("dragstart", function(){
+      draggedItem = item ;
+      setTimeout(() => {
+        item.style.display = "none";
+      }, 50);
+     })
+
+     item.addEventListener("dragend", function(){
+      setTimeout(() => {
+        draggedItem = null ;
+        item.style.display = "block";
+      }, 50);
+     })
+
+     for(let b = 0 ; b < lists.length ; b++){
+      const list = lists[b];
+
+      list.addEventListener("dragover" , function(e){
+              e.preventDefault();
+      })
+  
+      list.addEventListener("dragenter" , function(e){
+           e.preventDefault();
+           list.style.backgroundColor = 'rgba(255 , 255 , 255 , 0.7)';
+      })
+
+      list.addEventListener("dragleave" , function(){
+        list.style.backgroundColor = 'rgba(88, 65, 83, 0.5)';
+      })
+
+      list.addEventListener("drop" ,function(){
+        list.append(draggedItem);
+        list.style.backgroundColor = 'rgba(88, 65, 83, 0.5)';
+      })
+     }
+}
